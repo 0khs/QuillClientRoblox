279b49c9772a0de8ee79f775df1645b1ef81aa6c
0khs <140200653+0khs@users.noreply.github.com>
2025-04-19 20:23:30 +0800

    Update again because last one has too many bugs

--- QuillGui.lua
+++ QuillGui.lua
@@ -5,20 +5,23 @@
 ---]
 
 local quill = {
-	Version = "q0.1",
-	Keybind = "RightShift"
+	Version = "0.0.1",
+	Keybind = "LeftControl"
 }
 
 local uipallet = {
 	main = Color3.fromRGB(20, 17, 20),
-	header = Color3.fromRGB(25, 22, 26),
 	mainLight = "",
-	headerLight = ""
 }
 
+--------------------------------------------------
 local inputService = game:GetService("UserInputService")
 local guiService = game:GetService("GuiService")
 local playerGui = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+local assetTable = {
+	["quill/assets/Logo.png"] = "rbxassetid://111732193236676"
+}
 
 local function addCorner(parent, radius)
 	local Corner = Instance.new('UICorner')
@@ -27,14 +30,14 @@ local function addCorner(parent, radius)
 	
 	return addCorner
 end
-----------------------------------------------------
+
 local function makeDraggable(dragHandle, dragTarget)
 	local dragging = false
 	local dragStart = nil
 	local startPos = nil
-
+	
 	dragHandle.InputBegan:Connect(function(input)
-		if input.UserInputType == Enum.UserInputType.MouseButton1 then
+		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
 			dragging = true
 			dragStart = input.Position
 			startPos = dragTarget.Position
@@ -48,7 +51,7 @@ local function makeDraggable(dragHandle, dragTarget)
 	end)
 
 	inputService.InputChanged:Connect(function(input)
-		if dragging and input.UserInputType == Enum.UserInputType.MouseMovement then
+		if dragging and input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
 			local delta = input.Position - dragStart
 			dragTarget.Position = UDim2.new(
 				startPos.X.Scale,
@@ -64,16 +67,94 @@ end
 local gui = Instance.new('ScreenGui')
 gui.Name = "QuillGui"
 gui.DisplayOrder = 99999
+gui.ClipToDeviceSafeArea = true
+gui.IgnoreGuiInset = true
 gui.Parent = playerGui
 local mainframe = Instance.new('CanvasGroup')
 mainframe.Name = "mainframe"
-mainframe.Size = UDim2.new(0.45, 0, 0.6, 0)
+mainframe.Size = UDim2.new(0.45, 0, 0.65, 0)
 mainframe.Visible = true
 mainframe.AnchorPoint = Vector2.new(0.5, 0.5)
 mainframe.Position = UDim2.new(0.5, 0, 0.5, 0)
 mainframe.BorderSizePixel = 0
 mainframe.BackgroundColor3 = uipallet.main
+mainframe.ZIndex = 1
 mainframe.Parent = gui
-
 addCorner(mainframe, 8)
-makeDraggable(mainframe, mainframe)
+local header = Instance.new('Frame')
+header.Name = "header"
+header.Size = UDim2.new(1, 0, 0.1, 0)
+header.BorderSizePixel = 0
+header.Visible = true
+header.ZIndex = 2
+header.BackgroundTransparency = 1
+header.Parent = mainframe
+makeDraggable(header, mainframe)
+local guilogo = Instance.new('ImageLabel')
+guilogo.Name = "QuillLogo"
+guilogo.Size = UDim2.new(0.068, 0, 1, 0)
+guilogo.Image = assetTable["quill/assets/Logo.png"]
+guilogo.Visible = true
+guilogo.Position = UDim2.new(0.02, 0, 0.1, 0)
+guilogo.BackgroundTransparency = 1
+guilogo.Parent = header
+local guitext = Instance.new('TextLabel')
+guitext.Name = "QuillText"
+guitext.Size = UDim2.new(0.062, 0, 1, 0)
+guitext.Text = "QuilL"
+guitext.FontFace = Font.fromName("SourceSans", Enum.FontWeight.Thin, Enum.FontStyle.Normal)
+guitext.TextSize = 18
+guitext.TextColor3 = Color3.fromRGB(255, 255, 255)
+guitext.Visible = true
+guitext.Position = UDim2.new(0.076, 0, 0.12, 0)
+guitext.BackgroundTransparency = 1
+guitext.Parent = header
+local guiversion = Instance.new('TextLabel')
+guiversion.Name = "QuillVersion"
+guiversion.Size = UDim2.new(0.045, 0, 0.768, 0)
+guiversion.Text = quill.Version
+guiversion.TextColor3 = Color3.fromRGB(255, 255, 255)
+guiversion.FontFace = Font.fromName("Code", Enum.FontWeight.ExtraLight, Enum.FontStyle.Normal)
+guiversion.Visible = true
+guiversion.TextSize = 12
+guiversion.Position = UDim2.new(0.029, 0, 9.202, 0)
+guiversion.BackgroundTransparency = 1
+guiversion.Parent = header
+local mobilebutton = Instance.new('TextButton')
+mobilebutton.Name = "quill"
+mobilebutton.Size = UDim2.new(0, 60, 0, 20)
+mobilebutton.Position = UDim2.new(0.768, 0, 0.014, 0)
+mobilebutton.Text = "quill"
+mobilebutton.BackgroundTransparency = 0.2
+mobilebutton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+mobilebutton.TextColor3 = Color3.fromRGB(255, 255, 255)
+mobilebutton.Parent = gui
+
+mobilebutton.MouseButton1Click:Connect(function()
+	if mainframe.Visible == false then
+		mainframe.Visible = true
+	else
+		mainframe.Visible = false
+	end
+end)
+
+local ismobile = inputService.TouchEnabled
+
+if ismobile == true then
+	mobilebutton.Visible = true
+else
+	mobilebutton.Visible = false
+end
+
+inputService.InputBegan:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.Keyboard then
+		if input.KeyCode == Enum.KeyCode[quill.Keybind] then
+			if mainframe.Visible == false then
+				mainframe.Visible = true
+			else
+				mainframe.Visible = false
+			end
+		end
+	end
+end)
+
